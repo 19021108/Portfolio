eafd9184b081d4e9d9b422cb147167765bce084e
Superman <56293809+19021108@users.noreply.github.com>
2024-02-18 10:30:29 +0000

    Update Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Superman",
+  title: "Developer",
+  email: "superman@github.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "superman",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "Superman",
 };
 
 const primaryColor = "#4E567E";
